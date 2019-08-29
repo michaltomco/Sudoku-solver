7ca2035bec1707039c442e3361d78a7444524838
michaltomco <tomco.mito@gmail.com>
2019-08-29 10:37:12 +0200

    added source to master

---
+++ src/main/java/solution/SudokuBoard.java
@@ -0,0 +1,14 @@
+package solution;
+
+import java.awt.*;
+import java.util.List;
+
+public interface SudokuBoard {
+    boolean isNumberCorrectlyPlaced(Point coordinates);
+    void printBoard();
+    List<Point> getAllVariableCoordinates();
+    void writeCharacterTo(SudokuCharacter character, Point coordinates);
+    SudokuCharacter getCharacter(Point coordinates);
+    SudokuCharacter getIncrementedCharacter(Point coordinates);
+    SudokuCharacter getEmptyCharacter();
+}
